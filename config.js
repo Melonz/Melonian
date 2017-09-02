61bfec3baa8f462a8285f7f94dfa0ba608820b1e
Michael Webb <TheRandomMelon@users.noreply.github.com>
2017-09-02 18:48:53 -0500

    Create example config

---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+   This file is an example config. Tweak it to your liking.
+*/
+module.exports = {
+    token: "token here", // Your bot's token (can be found at https://discordapp.com/developers/applications/me)
+    maintainers: ["155432796247162881"], // "Maintainers" (has all permissions) - Array of user IDs
+    prefix: "m!", // The prefix for all commands. (e.g m!help)
+    desc: "[WIP] A lightweight-ish Eris bot, made for every Discord guild." // Description that will be in the help command
+};
