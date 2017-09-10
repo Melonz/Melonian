5b8d89726b0b4558e80d38bc45fe97ba90ad64ea
Michael Webb <trm@melonz.nl>
2017-09-10 13:35:06 -0500

    Fixed eval command to remove bot.token

--- commands/eval.js
+++ commands/eval.js
@@ -8,9 +8,11 @@ module.exports = (msg, args) => {
 				msgcontent = msgcontent.substring(5, msgcontent.length - 3);
 			}
 			const asyncCode = code => `(async () => {\nreturn ${code}\n})()`;
+			msgcontent = msgcontent.replace(new RegExp(`bot.token|config.token`, "g"), "nuh-uh sonny boy no token 4 u");
 			let result = eval(asyncCode(msgcontent));
 			if (typeof result !== "string") result = require("util").inspect(result, false, 2);
 			result = result.replace(new RegExp(`${bot.token}|${config.token}`, "g"), "nuh-uh sonny boy no token 4 u");
+			
 			msg.channel.createMessage({
 				embed: {
 					description: `\`\`\`js\n${result}\`\`\``,
