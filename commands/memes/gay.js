f67861d18f18b333f5e852562d4da0445f86683e
Michael Webb <trm@melonz.nl>
2017-09-15 18:18:50 -0500

    WOW I FORGOT TO PUT 'else'

--- commands/memes/gay.js
+++ commands/memes/gay.js
@@ -22,8 +22,9 @@ module.exports = (msg, args) => {
 				msg.channel.createMessage("Erm, he's 38% gai " + msg.author.mention);
 			} else if (personToTest === "<@155430797694861312>" || personToTest === "SonicHack" || personToTest.toUpperCase() === "Sonic") {
 				msg.channel.createMessage("I don't know how this is possible, but he's -24% gai " + msg.author.mention);
+			} else {
+				msg.channel.createMessage("I don't know " + personToTest + "... I'll guess that they're " + Math.floor((Math.random() * 101) + 1) + "% gai");
 			}
-			msg.channel.createMessage("I don't know " + personToTest + "... I'll guess that they're " + Math.floor((Math.random() * 101) + 1) + "% gai");
 		}
 	}
 };
