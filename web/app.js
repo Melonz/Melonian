8dc165f9ee91469473f268435eb53780d4cd78b9
TheRandomMelon <trm@melonz.nl>
2017-11-24 14:46:40 -0600

    Headers that DONT BREAK THE SITE :)#

--- web/app.js
+++ web/app.js
@@ -28,11 +28,15 @@ module.exports = function(bot, config) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'partials')));
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
   app.get('/', (req, res, next) => { 
     let os = require("os");
-  
-    res.setHeader('Set-Cookie', header)
 
     res.render('index.ejs', { title: 'Home', os: os, bot: bot, config: config, authUser: req.user, successlogout: req.query.successlogout });
   });
