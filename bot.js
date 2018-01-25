520ee06a060eb5b75bd4d4aa1ce6792eb3e22f8a
aleksanderxh <33936883+aleksanderxh@users.noreply.github.com>
2018-01-25 20:58:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,5 +6,5 @@ const config = require("./config.json");
 
 bot.on("ready", function() {
   console.log(`Ready!`)
-  client.user.setActivity(`with ${bot.users.size} users!`)
+  client.user.setActivity(`with ${bot.users.size} users!`) // Set the bot's game to how many users are in the servers that the bot is invited in
 })
