0ef20b97b029af65e29164ac1ac3b688d635399c
Michael Webb <trm@melonz.nl>
2018-04-05 21:06:14 -0500

    Lint and use embed from PR #11 (and also SR89 don't dismiss my reviews xx)

--- bot.js
+++ bot.js
@@ -47,7 +47,17 @@ client.on("message", async msg => {
 		console.log(err);
 		return null;
 	}
-	return cmdFile(client, msg, suffix, config);
+	return cmdFile(client, msg, suffix, config).catch(err => {
+		msg.channel.send({ embed: {
+			title: `Oh uh...`,
+			description: `We've hit an error. Here's what went wrong:\n\`\`\`${err}\`\`\``,
+			footer: {
+				text: `Contact a maintainer for support`,
+			},
+			color: 0xff0000,
+		},
+		});
+	});
 });
 
 client.on("guildCreate", async guild => {
